093bae298b07d8ba867d64d5e0ae517a2b170d67
gsskk <1080807+gsskk@users.noreply.github.com>
2026-05-18 19:37:33 +1000

    chore: bump version to 0.3.1

--- nonebot_plugin_hermes/__init__.py
+++ nonebot_plugin_hermes/__init__.py
@@ -14,7 +14,7 @@ require("nonebot_plugin_localstore")
 
 from .config import Config, plugin_config
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __plugin_meta__ = PluginMetadata(
     name="Hermes Agent",
