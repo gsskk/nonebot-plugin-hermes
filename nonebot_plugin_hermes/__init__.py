1d42385f8e732239fca32958f7c4694bc528f2f9
gsskk <1080807+gsskk@users.noreply.github.com>
2026-08-22 21:36:23 +1000

    chore(release): 0.5.2

--- nonebot_plugin_hermes/__init__.py
+++ nonebot_plugin_hermes/__init__.py
@@ -14,7 +14,7 @@ require("nonebot_plugin_localstore")
 
 from .config import Config, plugin_config
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 __plugin_meta__ = PluginMetadata(
     name="Hermes Agent",
